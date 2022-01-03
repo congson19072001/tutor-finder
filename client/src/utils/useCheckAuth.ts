46ded0f4956c5d9227fc77782079aecec2c99d13
Nguyễn Văn Công Sơn <congson1907vn@gmail.com>
2022-01-03 11:21:31 +0700

    fix

--- client/src/utils/useCheckAuth.ts
+++ client/src/utils/useCheckAuth.ts
@@ -6,8 +6,14 @@ const useCheckAuth = () => {
     const router = useRouter()
     const {data, loading} = useMeQuery();
     useEffect(() => {
-        if(!loading && data?.me && (router.route == '/login' || router.route == '/register' || router.route == '/forgot-password'|| router.route == '/change-password')) {
+        if(!loading && data?.me && (router.route === '/login' || router.route === '/register' || router.route === '/forgot-password'|| router.route === '/change-password')) {
             router.replace('/')
+        } else if(
+            !data?.me &&
+            router.route !== '/login' &&
+            router.route !== '/register'
+        ){
+            router.replace('/login')
         }
     }, [data, loading, router])
     return {data, loading}
